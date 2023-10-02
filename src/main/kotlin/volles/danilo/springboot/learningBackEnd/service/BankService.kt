78daa59ba14dccb1f2d78d9b10f1729977a88c40
Danilo Volles <danilo.volles@outlook.com>
2023-10-02 09:46:54 -0300

    Bank service and its tests implementation

---
+++ src/main/kotlin/volles/danilo/springboot/learningBackEnd/service/BankService.kt
@@ -0,0 +1,10 @@
+package volles.danilo.springboot.learningBackEnd.service
+
+import org.springframework.stereotype.Service
+import volles.danilo.springboot.learningBackEnd.datasource.BankDataSource
+import volles.danilo.springboot.learningBackEnd.model.Bank
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
